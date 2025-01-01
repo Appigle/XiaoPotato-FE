8afb0935d981f57b64143078cad76904da09672a
Chenley <chenley@qq.com>
2025-01-01 14:00:41 -0500

    chore: update fireworks link

--- src/App.tsx
+++ src/App.tsx
@@ -93,7 +93,10 @@ function App() {
   });
 
   const goToFirework = () => {
-    Number(firework) === 2025 && window.location.replace('https://www.xiaopotato.top/fireworks/');
+    Number(firework) > 0 &&
+      window.location.replace(
+        `https://www.xiaopotato.top/fireworks?year=${new Date().getFullYear() + 1}`,
+      );
   };
 
   const isGodMode = () => {
